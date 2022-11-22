97633d810a98821bd14deaf69b964e2336fad583
azul182 <84513231+azul182@users.noreply.github.com>
2022-11-22 09:55:09 -0300

    Delete 2emia-projeto-main directory

--- 2emia-projeto-main/config/database.js
+++
@@ -1,8 +0,0 @@
-//importar o mongoose
-const mongoose = require('mongoose')
-//scritps de conexao 
-const conn = async()=>{
-    const atlas = await mongoose.connect('mongodb+srv://UserAdmin:oliver123@redesocial.bxeqlaj.mongodb.net/Hubber')
-}
-//exportar as informacoes para acesso externo 
-module.exports = conn
